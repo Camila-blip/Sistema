1354d54ba6ebbced1c2d810e7114c62868765a75
Camila <millahrodriguez@outlook.com>
2022-03-25 18:09:52 -0300

    Buscando chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,67 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 import Header from "../../components/Header";
 import Title from '../../components/Title';
-import { useState } from "react";
+import firebase from '../../services/firebaseConnection';
 import { Content } from '../../Estilizacao/headerStyle';
 import { Container, DashboardContainer, LinkNew, Table } from '../../Estilizacao/dashboardStyle';
 import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi';
-
+import { format } from 'date-fns';
+const listRef =  firebase.firestore().collection('chamados').orderBy('created', 'desc');
 export default function Dashboard(){
-    const [chamados, setChamados] = useState([1]);
+
+    const [chamados, setChamados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+
+    useEffect(()=>{
+        loadChamados();
+        return() => {
+
+        }
+    }, []);
+
+    async function loadChamados(){
+        await listRef.limit(5)
+        .get()
+        .then((snapshot)=>{
+            updateState(snapshot);
+        })
+        .catch((error)=>{
+            console.log(error);
+            setLoadingMore(false);
+        })
+
+        setLoading(false);
+    }
+
+    async function updateState(snapshot){
+        const isCollectionEMpty = snapshot.size === 0;
+        if(!isCollectionEMpty){
+            let lista = [];
+            snapshot.forEach((doc)=>{
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clienteId: doc.data().clienteid,
+                    create: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento
+                })
+            })
+            const lastDoc = snapshot.docs[snapshot.doc.length -1];
+
+            setChamados(chamados => [...chamados, ...lista]);
+            setLastDocs(lastDoc);
+        }else{
+            setIsEmpty(true);
+        }
+        setLoadingMore(false);
+    }
 
     return(
         <section>
